88edb97c5f37e7cd9dcf6658f9f39e1ccca0f423
JonathanBechtel <JonathanBechtel@gmail.com>
2023-08-20 16:56:06 -0400

    Update data.py

--- src/util/data.py
+++ src/util/data.py
@@ -171,13 +171,3 @@ def convert_tsf_to_dataframe(
             contain_missing_values,
             contain_equal_length,
         )
-
-
-# Example of usage
-# loaded_data, frequency, forecast_horizon, contain_missing_values, contain_equal_length = convert_tsf_to_dataframe("TSForecasting/tsf_data/sample.tsf")
-
-# print(loaded_data)
-# print(frequency)
-# print(forecast_horizon)
-# print(contain_missing_values)
-# print(contain_equal_length)
